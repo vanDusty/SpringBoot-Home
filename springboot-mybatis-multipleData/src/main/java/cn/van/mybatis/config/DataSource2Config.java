2a185d3e62a9603368159a7ecc7000c54efba67e
Van <1251787298@qq.com>
2019-03-29 17:40:02 +0800

    SpringBoot Mybatis配置多数据源

---
+++ springboot-mybatis-multipleData/src/main/java/cn/van/mybatis/config/DataSource2Config.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: DataSource1Config
+ * Author:   zhangfan
+ * Date:     2019-03-29 15:03
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package cn.van.mybatis.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author zhangfan
+ * @create 2019-03-29
+ * @since 1.0.0
+ */
+@Configuration
+@MapperScan(basePackages = "cn.van.mybatis.dao.test2", sqlSessionTemplateRef  = "test2SqlSessionTemplate")
+public class DataSource2Config {
+
+    @Bean(name = "test2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.test2")
+    public DataSource testDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "test2SqlSessionFactory")
+    public SqlSessionFactory testSqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/test2/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean(name = "test2TransactionManager")
+    public DataSourceTransactionManager testTransactionManager(@Qualifier("test2DataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "test2SqlSessionTemplate")
+    public SqlSessionTemplate testSqlSessionTemplate(@Qualifier("test2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
---
+++ springboot-mybatis-multipleData/src/test/java/cn/van/mybatis/MybatisTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: MybatisTest
+ * Author:   zhangfan
+ * Date:     2019-03-29 12:00
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package cn.van.mybatis;
+import cn.van.mybatis.dao.test1.User1Mapper;
+import cn.van.mybatis.dao.test2.User2Mapper;
+import cn.van.mybatis.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author zhangfan
+ * @create 2019-03-29
+ * @since 1.0.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MybatisTest {
+
+
+    @Resource
+    User1Mapper user1Mapper;
+    @Resource
+    User2Mapper user2Mapper;
+    @Test
+    public void insert() {
+        System.out.println(111);
+        User user = new User();
+        user.setUserName("van");
+        user.setPassWord("222");
+        user.setUserSex("eeee");
+        user1Mapper.insert(user);
+    }
+    @Test
+    public void insert2() {
+        System.out.println(111);
+        User user = new User();
+        user.setUserName("van");
+        user.setPassWord("222");
+        user.setUserSex("eeee");
+        user2Mapper.insert(user);
+    }
+}
