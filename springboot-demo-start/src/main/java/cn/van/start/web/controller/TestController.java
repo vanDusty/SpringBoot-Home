95bcc05b2ab401cb9dbb22cabd304a3e16d38ae9
Van <1251787298@qq.com>
2019-07-25 15:32:01 +0800

    SpringBoot(一)：构建第一个SpringBoot工程

---
+++ springboot-demo-start/src/main/java/cn/van/start/web/controller/TestController.java
@@ -0,0 +1,26 @@
+package cn.van.start.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (C), 2015-2019, 风尘博客
+ * 公众号 : 风尘博客
+ * FileName: TestController
+ *
+ * @author: Van
+ * Date:     2018-12-11 15:07
+ * Description: ${DESCRIPTION}
+ * Version： V1.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @RequestMapping("/sayHello")
+    public String sayHello() {
+        return "Hello World !";
+    }
+
+}
