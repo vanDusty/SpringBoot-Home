2314b884910ddedea8730faf9a4e35558af4c2a3
Van <1251787298@qq.com>
2021-10-28 00:31:39 +0800

    Spring Boot 启动动图

---
+++ springboot-demo-funny/src/main/java/cn/van/funny/FunnyApplication.java
@@ -0,0 +1,12 @@
+package cn.van.funny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyApplication.class, args);
+    }
+
+}
